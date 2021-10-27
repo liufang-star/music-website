4117f9c90ef7d9a837e9301497e221ae956498d9
liufang-star <2531994628@qq.com>
2021-10-27 11:59:02 +0800

    first commit

---
+++ music-manage/src/api/config.js
@@ -0,0 +1,2 @@
+// export const BASE_URL = 'http://121.37.71.154:8885'
+export const BASE_URL = 'http://127.0.0.1:8885'
---
+++ music-server/src/main/java/com/example/yin/constant/Constants.java
@@ -0,0 +1,9 @@
+package com.example.yin.constant;
+
+public class Constants {
+    // MAC、Linux系统
+    public static final String RESOURCE_MAC_PATH = "/www/wwwroot/default/Yin/music";
+
+    // windos系统
+    public static final String RESOURCE_WIN_PATH = "G:\\idea_project_springboot\\music-website-master\\music-server";
+}
